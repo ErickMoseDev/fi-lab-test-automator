b0cd2fb8208f48cf132a52b8b9bd32724ba53773
ErickMoseDev <erickmongaremose@gmail.com>
2022-08-30 11:38:40 +0300

    ui-changes

--- README.md
+++ README.md
@@ -1,12 +1,39 @@
 # FI Labs Automator
+
 This is an application that automates lab tests, outputs the results and cleans the cloned repository.
 
 ### How to use
+
 To use it remotely visit https://fi-labs-automator.herokuapp.com/
 or if you like, you can download it and install it on you machine by runing
+
 ```
 git clone https://github.com/njmwasmoringa/fi-lab-test-automator
 cd fi-lab-test-automator
 npm install
 npm start
 ```
+
+<p>Welcome to <%= title %>
+    </p>
+
+    <h3>Lab Repository</h3>
+    <p>Enter the lab repository link below</p>
+    <form onsubmit="runtest(event)">
+      <div class="row">
+        <div class="col-9">
+          <input type="url" name="repolink" class="form-control" id="repolink" placeholder="Repository Link: https://"
+            required />
+        </div>
+
+        <div class="col-3">
+          <button type="submit" class="btn btn-primary" id="submitButton">Submit Link</button>
+        </div>
+      </div>
+      <div class="link">
+        <h6>Link Submission</h6>
+        <div class="wrapper"></div>
+        <div class="wrapper"></div>
+      </div>
+      <div id="output"></div>
+    </form>
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,4 @@
-function runtest(evt){
+function runtest(evt) {
     evt.preventDefault();
     const form = evt.target;
     const link = form.repolink.value;
@@ -6,29 +6,31 @@ function runtest(evt){
     document.getElementById('submitButton').disabled = true;
     document.getElementById('output').innerHTML = 'Running the tests, please wait...';
     fetch('/', {
-        method:'POST', 
+        method: 'POST',
         body: `{"repolink":"${link}"}`,
-        headers:{
-            "Content-Type":"application/json"
+        headers: {
+            "Content-Type": "application/json"
         }
     })
-    .then(r=>r.json())
-    .then(resp=>{
-        if(resp.error){
-            throw new Error(resp.error);
-        }
-        document.getElementById('output').innerHTML = resp.data.replace(/\n/g, "<br />");
-        console.log(resp);
-    })
-    .catch(e=>{
-        console.log(e.message);
-        document.getElementById('output').innerHTML = `${e.message}`;
-    })
-    .finally(()=>{
-        document.getElementById('submitButton').disabled = false;
-    });
+        .then(r => r.json())
+        .then(resp => {
+            if (resp.error) {
+                throw new Error(resp.error);
+            }
+            document.getElementById('output').innerHTML = resp.data.replace(/\n/g, "<br />");
+            console.log(resp);
+        })
+        .catch(e => {
+            console.log(e.message);
+            document.getElementById('output').innerHTML = `${e.message}`;
+            document.getElementById('repolink').value = '';
+        })
+        .finally(() => {
+            document.getElementById('submitButton').disabled = false;
+            document.getElementById('repolink').value = '';
+        });
 }
 
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     console.log("Content is ready")
 });
